df0dc7a8e543956acb1e561886bca988e98b8b35
Borislav Napoleonov <borislav.napoleonov@aveva.com>
2024-10-07 03:36:16 +0100

    adjust navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="min-h-screen bg-gray-100">
     <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
+      <div class="max-w-8xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
         <h1 class="text-4xl font-bold text-gray-900 flex items-center">
           <span>DevTest</span><span class="text-blue-600">Cube</span>
         </h1>
